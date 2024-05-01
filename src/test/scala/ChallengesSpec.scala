2379e2321cd1f5e8cc919257882bb6e1e79ea460
Makarov Andrey <winandfx@mail.ru>
2024-05-01 18:58:19 +0000

    02 challenges and tests

--- src/test/scala/ChallengesSpec.scala
+++ src/test/scala/ChallengesSpec.scala
@@ -38,4 +38,89 @@ class ChallengesSpec extends AnyFreeSpec with Matchers {
       result shouldBe empty
     }
   }
+
+  "applyFunction" - {
+    "applies a function to an integer" in {
+      val square: Int => Int = num => num * num
+      val result = applyFunction(square, 5)
+      result shouldBe 25
+    }
+
+    "applies a function to a string" in {
+      val greet: String => String = name => s"Hello, $name!"
+      val result = applyFunction(greet, "Akin")
+      result shouldBe "Hello, Akin!"
+    }
+
+    "applies a function to a case class" in {
+      case class Person(name: String, age: Int)
+      val getDescription: Person => String =
+        person => s"${person.name} is ${person.age} years old."
+      val person = Person("Coco", 65)
+      val result = applyFunction(getDescription, person)
+      result shouldBe "Coco is 65 years old."
+    }
+  }
+
+  "processPayment" - {
+    "processes successful payment" in {
+      val amount = 50.0
+      val cardBalance = 100.0
+      val result = processPayment(amount, cardBalance)
+      result shouldBe Right(50.0)
+    }
+
+    "handles insufficient balance" in {
+      val amount = 150.0
+      val cardBalance = 100.0
+      val result = processPayment(amount, cardBalance)
+      result shouldBe Left("Insufficient balance")
+    }
+  }
+  "getWeatherDescription" - {
+    "should describe the weather conditions correctly" - {
+      "for Sunny weather" in {
+        val description = getWeatherDescription(WeatherCondition.Sunny)
+        description shouldBe "It's a sunny day."
+      }
+
+      "for Cloudy weather" in {
+        val description = getWeatherDescription(WeatherCondition.Cloudy)
+        description shouldBe "It's a cloudy day."
+      }
+
+      "for Rainy weather" in {
+        val description = getWeatherDescription(WeatherCondition.Rainy)
+        description shouldBe "It's a rainy day."
+      }
+
+      "for Snowy weather" in {
+        val description = getWeatherDescription(WeatherCondition.Snowy)
+        description shouldBe "It's a snowy day."
+      }
+    }
+  }
+
+  "Notifications" - {
+    "EmailNotification" in {
+      val emailAddress = "user@example.com"
+      val emailNotification = EmailNotification(emailAddress)
+      val message = "Hello, this is an email"
+      val formattedMessage = emailNotification.formatMessage(message)
+      val result = emailNotification.sendNotification(message)
+
+      formattedMessage shouldBe s"Message: $message"
+      result shouldBe s"Sending email to $emailAddress with message: $message"
+    }
+    "SMSNotification" in {
+      val phoneNumber = "07525015566"
+      val smsNotification = SMSNotification(phoneNumber)
+      val message = "Hello, this is an SMS"
+      val formattedMessage = smsNotification.formatMessage(message)
+      val result = smsNotification.sendNotification(message)
+
+      formattedMessage shouldBe s"Message: $message"
+      result shouldBe s"Sending SMS to $phoneNumber: $message"
+    }
+  }
 }
